e96d30d55f5a8273183c0bdd1956932c393d37b5
hesauhugo <hesau.hugo@hotmail.com>
2024-04-05 23:54:08 -0300

    feat: testes de unidade mergesort e insertion sort

---
+++ dio-desafio-tdd.test/AlgoritmosDeOrdenacaoTest.cs
@@ -0,0 +1,66 @@
+using System.Runtime.InteropServices;
+
+namespace dio_desafio_tdd.Tests
+{
+    public class AlgoritmosDeOrdenacaoTest
+    {
+        [Theory]
+        [InlineData(new int[] { 5, 2, 4, 6, 1, 3 })]
+        [InlineData(new int[] { 9, 8, 7, 6, 5, 4,3,2,1 })]
+        [InlineData(new int[] {10,4,1,2,3,5,9,7,8,6})]
+        public void ArranjoDesordenado_OrdenarPeloInsertion_DeveSerValido(int[] arranjoDesordenado)
+        {
+            //Arrange
+            //inlineData
+            int[] arranjoTest = new int[arranjoDesordenado.Length];
+            Array.Copy(arranjoDesordenado, arranjoTest, arranjoDesordenado.Length);
+
+            //Act
+            var algoritmosDeOrdenacao = new AlgoritmosDeOrdenacao();
+            var arranjoOrdenado = algoritmosDeOrdenacao.InsertionSort(arranjoTest);
+
+            //Assert
+            Assert.Equal(arranjoDesordenado.OrderBy(a => a), arranjoOrdenado);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 2,4,5,7,1,2,3,6 },0, 3,7) ]
+        [InlineData(new int[] { 3,4 ,6, 1, 2, 5 },0,2,5)]
+        [InlineData(new int[] { 8, 10, 16, 1, 3, 9 },0,2,5)]
+        public void ArranjoDesordenado_OrdenarPeloMerge_DeveSerValido(int[] arranjoDesordenado,int p, int q, int r)
+        {
+            //Arrange
+            //inlineData
+            int[] arranjoTest = new int[arranjoDesordenado.Length];
+            Array.Copy(arranjoDesordenado, arranjoTest, arranjoDesordenado.Length);
+
+            //Act
+            var algoritmosDeOrdenacao = new AlgoritmosDeOrdenacao();
+            var arranjoOrdenado = algoritmosDeOrdenacao.Merge(arranjoTest, p,q,r);
+
+            //Assert
+            Assert.Equal(arranjoDesordenado.OrderBy(a => a), arranjoOrdenado);
+
+        }
+
+        [Theory]
+        [InlineData(new int[] { 2, 4, 5, 7, 1, 2, 3, 6 }, 0, 7)]
+        [InlineData(new int[] { 3, 4, 6, 1, 2, 5 }, 0, 5)]
+        [InlineData(new int[] { 8, 10, 16, 1, 3, 9 }, 0, 5)]
+        public void ArranjoDesordenado_OrdenarPeloMergeSort_DeveSerValido(int[] arranjoDesordenado, int p, int r)
+        {
+            //Arrange
+            //inlineData
+            int[] arranjoTest = new int[arranjoDesordenado.Length];
+            Array.Copy(arranjoDesordenado, arranjoTest, arranjoDesordenado.Length);
+
+            //Act
+            var algoritmosDeOrdenacao = new AlgoritmosDeOrdenacao();
+            var arranjoOrdenado = algoritmosDeOrdenacao.MergeSort(arranjoTest, p, r);
+
+            //Assert
+            Assert.Equal(arranjoDesordenado.OrderBy(a => a), arranjoOrdenado);
+
+        }
+    }
+}
---
+++ dio-desafio-tdd/AlgoritmosDeOrdenacao.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dio_desafio_tdd
+{
+    public class AlgoritmosDeOrdenacao
+    {
+        public int[] InsertionSort(int[] A)
+        {
+            for (int j = 1; j < A.Length; j++)
+            {
+                var chave = A[j];
+                var i = j - 1;
+
+                while (i > -1 && A[i] > chave)
+                {
+                    A[i + 1] = A[i];
+                    i = i - 1;
+                }
+
+                A[i + 1] = chave;
+
+            }
+
+
+            return A;
+        }
+
+        public int[] Merge(int[] A,int p,int q, int r)
+        {
+            int n1 = q-p +1; //calcula o comprimento do subarranjo A[p..q]
+            int n2 = r - q; //calcula o comprimento do subarranjo A[q+1 ..r]
+
+            int[] L = new int[n1 + 1]; //cria arranjo da esquerda + posição extra para sentinela
+            int[] R = new int[n2 + 1]; //cria o arranjo da direita + posição extra para sentinela
+
+            //copiando o subarranjo da esquerda
+            for (int _i = 0; _i < n1; _i++)
+                L[_i] = A[p + _i];
+
+            //copiando o subarranjo da direita
+            for (int _j = 0; _j < n2; _j++)
+                R[_j] = A[q+1 + _j];
+
+            //acrescentando um valor grande para a posição de sentinela
+            L[n1] = int.MaxValue;
+            R[n2] = int.MaxValue;
+
+            int i = 0;
+            int j = 0;
+
+            for (int k = p; k <= r; k++)
+            {
+                if (L[i] <= R[j])
+                {
+                    A[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    A[k] = R[j];
+                    j++;
+                }
+            }
+
+            return A;
+        
+        }
+
+        public int[] MergeSort(int[] A, int p, int r)
+        {
+            if (p < r)
+            {
+                int q = (int)(Decimal.Truncate((p + r) / 2));
+                A=MergeSort(A, p, q);
+                A=MergeSort(A, q + 1, r);
+                A = Merge(A, p, q, r);
+            }
+
+            return A;
+        }
+
+    }
+}
